2ceeb4be2eb4a17492d9a820b5e02c8acf067217
Kidslasher7RN <revoshifu@gmail.com>
2026-01-23 16:19:54 +0700

    working on Index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 
 import "./App.css";
+
 import Navbar from "./components/Navbar";
 import Article from "./components/Article";
 
@@ -8,22 +9,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="App">
-        {/* <Navbar /> */}
-        <div className="Article-Container">
-          {ivyLeagueStudents.map((student) => {
-            return (
-              <Article
-                name={student.firstName}
-                institution={student.institution}
-                personalStatement={student.personalStatement}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </>
+    <div className="App">
+      <Homepage />
+    </div>
   );
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,21 @@
+import dummyDb from "../db.json";
+
 const Homepage = () => {
   return (
     <>
       <h1>Ivy's Words</h1>
+
+      <div className="Article-Container">
+        {dummyDb.map((student) => {
+          return (
+            <Article
+              name={student.firstName}
+              institution={student.institution}
+              personalStatement={student.personalStatement}
+            />
+          );
+        })}
+      </div>
     </>
   );
 };
