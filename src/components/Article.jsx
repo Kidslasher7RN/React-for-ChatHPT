f577555ed60b9f33fd3b14f8bfa308c5d6ef023c
Kidslasher7RN <revoshifu@gmail.com>
2026-01-26 22:10:57 +0700

    conditional rendering

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,20 @@
 import "./Article.css";
 
+const CumlaudeBadge = () => {
+  return (
+    <>
+      | <i>cum laude</i>
+    </>
+  );
+};
+
 const Article = (props) => {
   return (
     <div className="Article">
       <div className="head">
         <p>
-          {props.firstName} | {props.institution}
+          {props.firstName} | {props.institution}{" "}
+          {props.gpa > 3.9 && <CumlaudeBadge />}
         </p>
       </div>
       <div className="body">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,14 +2,11 @@ import {useState} from "react";
 
 const Search = (props) => {
   const [search, setSearch] = useState("");
-  const [resultCount, setResultCount] = useState(0);
 
   function onSearchChange(e) {
     const value = e.currentTarget.value;
     setSearch(value);
     props.onChange(value);
-
-    return props.onChange(value).length;
   }
 
   return (
